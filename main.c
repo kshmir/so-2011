01aee4517048a5a6215ebe85014e9a5a9512abef
Cristian Pereyra <criis.pereyra@gmail.com>
2011-08-02 14:52:13 -0300

    agregado ejemplo de threads #reviewthis @maximovs @ilmarse

---
+++ main.c
@@ -0,0 +1,11 @@
+/*
+ *  main.c
+ *  so-2011
+ *
+ *  Created by Cristian Pereyra on 02/08/11.
+ *  Copyright 2011 My Own. All rights reserved.
+ *
+ */
+
+#include "main.h"
+
---
+++ main.h
@@ -0,0 +1,16 @@
+/*
+ *  main.h
+ *  so-2011
+ *
+ *  Created by Cristian Pereyra on 02/08/11.
+ *  Copyright 2011 My Own. All rights reserved.
+ *
+ */
+
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{	
+	return 0;
+}
