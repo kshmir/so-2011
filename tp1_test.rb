9fbf6e3067cbd9b01cb048149bde4dcaebf044d9
Cristian Pereyra <criis.pereyra@gmail.com>
2011-09-07 12:46:46 -0300

    test maxi

--- tp1_test.rb
+++ tp1_test.rb
@@ -1,5 +1,5 @@
 c = 0;
   while c < 10 do
-    Process.waitpid(fork { exec("true && ./tp1.sh #{ARGV[0]} nocomp") })
+    Process.waitpid(fork { exec("./tp1 --method #{ARGV[0]} --level files/default/mapa.txt --airline files/default/aerolinea1.txt files/default/aerolinea2.txt files/default/aerolinea3.txt files/default/aerolinea4.txt files/default/aerolinea5.txt files/default/aerolinea6.txt files/default/aerolinea7.txt files/default/aerolinea8.txt files/default/aerolinea9.txt files/default/aerolinea0.txt ") })
     c += 1
   end
