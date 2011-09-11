d7772e041b398967c33919f5279f74ea26a6c02a
Cristian Pereyra <criis.pereyra@gmail.com>
2011-09-11 00:20:12 -0300

    test

--- tp1_test.rb
+++ tp1_test.rb
@@ -1,5 +1,5 @@
 c = 0;
-  while c < 10 do
+  while c < 3 do
     Process.waitpid(fork { exec("./tp1 --method #{ARGV[0]} --level files/default/mapa.txt --airline files/default/aerolinea1.txt files/default/aerolinea2.txt files/default/aerolinea3.txt files/default/aerolinea4.txt files/default/aerolinea5.txt files/default/aerolinea6.txt files/default/aerolinea7.txt files/default/aerolinea8.txt files/default/aerolinea9.txt files/default/aerolinea0.txt ") })
     c += 1
   end
